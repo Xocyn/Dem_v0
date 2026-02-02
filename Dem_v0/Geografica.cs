e2a86b3c4d642d07cdefd15882a017aeafd64feb
Nicolas Gutierrez <nicox.gutierrez@gmail.com>
2026-02-02 11:40:15 -0300

    agrega geografica

---
+++ Dem_v0/Geografica.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dem_v0
+{
+    internal class Geografica
+    {
+        public static void AreaGeografica(int i, string input)
+        {
+            // obtengo latitud (paralelo al ecuador)
+
+            // luego obtengo longitud (paralelo a greenwich)
+
+        }
+    }
+}
